2c8ed927dc8761b87b84065ca4b771a2d097a3a6
amitprp <amitprp@gmail.com>
2023-02-25 15:01:31 +0200

    try1

--- GitProblem/main.py
+++ GitProblem/main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __ ':
-    print_hi('PyCharm')
+    print_hi('PyCharm')rtggrg
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
